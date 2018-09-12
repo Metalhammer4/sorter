dc3b3e5b159e2c20a64f000e580ed0b7b5854170
Eric <ericmetalhammer@gmail.com>
2018-09-13 02:44:44 +0300

    'sort_array'

--- src/index.js
+++ src/index.js
@@ -1,31 +1,60 @@
 class Sorter {
+  
   constructor() {
-    // your implementation
+      this.arr = [];
+      this.compareNumbers = (a, b) => a - b;
+      this.compareFunction = this.compareNumbers;
+      const AGE_COMPARATOR = (left, right) => left.age - right.age;
+      const REVERSE_COMPARATOR = (left, right) => right - left;
+      const STRINGIFY_COMPARATOR = (left, right) => JSON.stringify(left, null, 2).length - JSON.stringify(right, null, 2).length; 
   }
-
+  
   add(element) {
-    // your implementation
+      this.arr.push(element); 
   }
 
   at(index) {
-    // your implementation
+      return this.arr[index];
   }
 
   get length() {
-    // your implementation
+      return this.arr.length;
   }
 
   toArray() {
-    // your implementation
+      return this.arr.slice();
   }
 
-  sort(indices) {
-    // your implementation
+  sort(indices) {  
+    
+    var temp =[];
+    indices.sort(this.compareNumbers);
+
+    for (var i = 0; i < indices.length; i++) {
+        if (indices[i] >= 0 && indices[i] < this.arr.length){
+            temp.push(this.arr[ indices[i] ]);
+        }
+        else{
+            console.log("Index: " + indices[i] + " outs of range!");
+        }
+    }
+    
+    var tempSize = temp.length;
+    temp.sort(this.compareFunction);
+    
+    for (var i = 0; i < tempSize; i++) {
+        this.arr[ indices[i] ] = temp.shift();
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+      if (compareFunction != null){
+          this.compareFunction = compareFunction;
+      } else {
+          this.compareFunction = this.compareNumbers;
+      }
   }
+
 }
 
 module.exports = Sorter;
